b1ebd9f388df559b83f06b2f326c86821ebdccd3
Bianca Canlas <bcanlas87@gmail.com>
2020-05-14 01:28:03 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,7 +12,8 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  create_an_array=["pink", "red", "blue", "green"]
+  create_an_array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
