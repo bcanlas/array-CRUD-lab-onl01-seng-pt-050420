81dea99bbf7f45d26e45000803e8cabc63aeb7b3
Bianca Canlas <bcanlas87@gmail.com>
2020-05-14 01:26:12 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   create_an_array=["pink", "red", "blue", "green"]
-  create_an_array.push("black")
+  create_an_array.push("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
